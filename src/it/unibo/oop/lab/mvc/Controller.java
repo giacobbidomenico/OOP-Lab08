810342b8949f3497aeac9cb50be4363bb100e4fd
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-23 10:11:26 +0100

    definition of the the interface Controller

--- src/it/unibo/oop/lab/mvc/Controller.java
+++ src/it/unibo/oop/lab/mvc/Controller.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lab.mvc;
 
+import java.util.List;
+
 /**
  * A controller that prints strings and has memory of the strings it printed.
  */
@@ -8,21 +10,32 @@ public interface Controller {
     /*
      * This interface must model a simple controller responsible of I/O access. It
      * considers only the standard output, and it is able to print on it.
-     * 
-     * Write the interface and implement it in a class in such a way that it
-     * includes:
-     * 
-     * 1) A method for setting the next string to print. Null values are not
+     */
+    /**
+     * A method for setting the next string to print. Null values are not
      * acceptable, and an exception should be produced
      * 
-     * 2) A method for getting the next string to print
+     * @param message the string to print 
+     */
+    void setNextIntStringToPrint(String message);
+    /**
+     * A method for getting the next string to print.
      * 
-     * 3) A method for getting the history of the printed strings (in form of a List
-     * of Strings)
+     * @return the next String to print
+     */
+    String getNextStringToPrint();
+    /**
+     * A method for getting the history of the printed strings (in form of a List of Strings).
      * 
-     * 4) A method that prints the current string. If the current string is unset,
+     * @return the history of printed Strings
+     */
+    List<String> getHistoryOfPrintedStrings();
+    /**
+     * A method that prints the current string. If the current string is unset,
      * an IllegalStateException should be thrown
      * 
+     * @return the current String
+     * @throws IllegalStateException
      */
-
+    String getCurrentString();
 }
