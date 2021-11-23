8b407b8e3b5f41a6447a92c1759acdc62f88df94
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-23 09:47:51 +0100

    update definition of the method attempt in the class DrawNumber

--- src/it/unibo/oop/lab/advanced/DrawNumber.java
+++ src/it/unibo/oop/lab/advanced/DrawNumber.java
@@ -17,8 +17,6 @@ public interface DrawNumber {
      * @param n
      *            the guess
      * @return the result of the guess
-     * @throws AttemptsLimitReachedException
-     *             in case the game is lost
      */
-    DrawResult attempt(int n) throws AttemptsLimitReachedException;
+    DrawResult attempt(int n);
 }
