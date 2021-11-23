b18268f554841ba2e541fc96b47ff39617bebbd2
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-11-23 09:50:04 +0100

    update javadoc of the the class OutputView

--- src/it/unibo/oop/lab/advanced/OutputView.java
+++ src/it/unibo/oop/lab/advanced/OutputView.java
@@ -4,58 +4,55 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 /**
- * 
- *
+ * Class that implements a view that write in a stream.
  */
 public class OutputView implements DrawNumberView {
     private final PrintStream stream;
     /**
+     * Builds a new {@link OutputView}.
      * 
-     * @param stream
+     * @param stream {@link PrintStream} where to write
      */
     public OutputView(final PrintStream stream) {
         this.stream = stream;
     }
     /**
+     * Builds a new {@link OutputView}.
      * 
-     * @param path
+     * @param path the path where is the file
      * @throws FileNotFoundException
      */
     public OutputView(final String path) throws FileNotFoundException {
         stream = new PrintStream(new File(path));
     }
     /**
-     * 
+     * {@inheritDoc} 
      */
     @Override
     public void setObserver(final DrawNumberViewObserver observer) {
     }
     /**
-     * 
+     * {@inheritDoc} 
      */
     @Override
     public void start() {
     }
     /**
-     * 
+     * {@inheritDoc} 
      */
     @Override
     public void numberIncorrect() {
         stream.println("Number incorrect!!!");
     }
     /**
-     * 
+     * {@inheritDoc}
      */
     @Override
     public void result(final DrawResult res) {
         stream.println("The result is: " + res);
     }
-
-    @Override
-    public void limitsReached() {
-    }
     /**
-     * 
+     * {@inheritDoc}
      */
     @Override
     public void displayError(final String message) {
